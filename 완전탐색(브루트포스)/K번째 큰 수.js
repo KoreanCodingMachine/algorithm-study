a65c1d7d4d5fda73d2fc33b5b3284d7503c66e86
kimjuhyeong <pon03078@naver.com>
2023-04-20 11:43:36 +0900

    k번째 큰 수

---
+++ 완전탐색(브루트포스)/K번째 큰 수.js
@@ -0,0 +1,22 @@
+function solution(n, k, card){
+    let answer;
+    let sum = []
+
+    // 카드 배열에서 3가지 카드를 임의로 뽑는 경우의 수
+    for (let i=0; i<card.length; i++){
+        for (let j=i+1; j<card.length; j++){
+            for (let k=j+1; k<card.length; k++){
+               sum.push(card[i]+card[j]+card[k])
+            }
+        }
+    }
+
+    // 큰 수로 정렬
+    sum.sort((a,b) => b-a)
+    
+
+    return answer = sum[k-1]
+}
+
+let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
